8b01c04de3a0138f6d0c4e62cf4eb9822ff5f5c3
viettq <viettq@batdongsan.com.vn>
2023-08-17 08:59:21 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.10.2-slim-buster
+WORKDIR /app
+COPY requirements.txt .
+RUN pip3 install -r requirements.txt
+COPY server.py .
+EXPOSE 5000
+ENTRYPOINT ["python", "server.py"]
---
+++ server.py
@@ -0,0 +1,64 @@
+import random
+import uvicorn
+import prometheus_client
+
+from fastapi import FastAPI, HTTPException, Response
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+heads_count = prometheus_client.Counter(
+    "heads_count",
+    "Number of heads",
+)
+
+tails_count = prometheus_client.Counter(
+    "tails_count",
+    "Number of tails",
+)
+
+flip_count = prometheus_client.Counter(
+    "flip_count",
+    "Number of flip",
+)
+
+@app.get("/flip-coins")
+async def flip_coins(times=None):
+    if times is None or not times.isdigit():
+        raise HTTPException(
+            status_code=400,
+            detail="Time must be integer",
+        )
+    times_as_int = int(times)
+
+    heads = 0
+    for _ in range(times_as_int):
+        if random.randint(0,1):
+            heads += 1
+    tails = times_as_int - heads
+
+    heads_count.inc(heads)
+    tails_count.inc(tails)
+    flip_count.inc(times_as_int)
+
+    return {
+        "heads": heads,
+        "tails": tails,
+    }
+
+@app.get('/metrics')
+def get_metrics():
+    return Response(
+        media_type="text/plain",
+        content=prometheus_client.generate_latest(),
+    )
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="0.0.0.0",port=5000)
